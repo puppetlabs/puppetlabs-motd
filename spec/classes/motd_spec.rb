77d78561359f431b1f3d3c960d8e86abc623ea15
Kyle Anderson <kyle@xkyle.com>
2013-10-19 20:17:59 -0700

    Added class paramater for external templates and tests

---
+++ spec/classes/motd_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'motd', :type => :class do
+
+  describe 'On a non-linux system' do
+    let(:facts) {{ :kernel => 'Unknown' }}
+    it "should not fail" do
+      expect do
+        subject
+      end.not_to raise_error()
+    end
+    it { should_not contain_file('/etc/motd') }
+  end
+
+  describe "On Linux" do
+    let(:facts) {{
+      :kernel          => 'Linux',
+      :operatingsystem => "TestOS",
+      :memoryfree      => "1 KB",
+      :domain          => "testdomain"
+    }}
+    context "When a motd template is not specified" do
+      it { should contain_File('/etc/motd').with(
+        :ensure  => 'file',
+        :backup  => 'false',
+        :content => "The operating system is TestOS\nThe free memory is 1 KB\nThe domain is testdomain\n"
+        )
+      }
+    end
+    context "When an external template is specified" do
+      let(:params) { { :template => 'motd/spec.erb' } }
+      it { should contain_File('/etc/motd').with(
+        :ensure  => 'file',
+        :backup  => 'false',
+        :content => "Test Template for Rspec\n"
+        ) 
+      }
+    end   
+  end
+
+end
+
