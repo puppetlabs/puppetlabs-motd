39f97de518c64f53239453e8a7681365d38c3889
Geoff Williams <geoff.williams@puppetlabs.com>
2016-12-13 14:45:05 +0800

    (#5955) Support for /etc/issue and /etc/issue.net
    Introduces new parameters: issue_template, issue_content, issue_net_template, issue_net_content - to control what if anything is written to /etc/issue and /etc/issue.net respectively.  Includes tests and documentation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,10 @@
 # @param dynamic_motd [Bool] Enable or disable dynamic motd on Debian systems
 # @param template [String] Allows for custom template location
 # @param content [String] String to be used for motd, priority given to template
+# @param issue_template [String] Allows for custom template location for /etc/issue
+# @param issue_content [String] String to be used for /etc/issue, priority given to template
+# @param issue_net_template [String] Allows for custom template location for /etc/issue.net
+# @param issue_net_content [String] String to be used for /etc/issue.net, priority given to template
 #
 # @example
 #  include motd
@@ -13,6 +17,10 @@ class motd (
   $dynamic_motd = true,
   $template = undef,
   $content = undef,
+  $issue_template = undef,
+  $issue_content = undef,
+  $issue_net_template = undef,
+  $issue_net_content = undef,
 ) {
 
   if $template {
@@ -26,12 +34,51 @@ class motd (
     $motd_content = template('motd/motd.erb')
   }
 
+  if $issue_template {
+    if $issue_content {
+        warning('Both $issue_template and $issue_content parameters passed to motd, ignoring issue_content')
+    }
+    $_issue_content = template($issue_template)
+  } elsif $issue_content {
+    $_issue_content = $issue_content
+  } else {
+    $_issue_content = false
+  }
+
+  if $issue_net_template {
+    if $issue_net_content {
+        warning('Both $issue_net_template and $issue_net_content parameters passed to motd, ignoring issue_net_content')
+    }
+    $_issue_net_content = template($issue_net_template)
+  } elsif $issue_net_content {
+    $_issue_net_content = $issue_net_content
+  } else {
+    $_issue_net_content = false
+  }
+
   if $::kernel == 'Linux' {
     file { '/etc/motd':
       ensure  => file,
       backup  => false,
       content => $motd_content,
     }
+
+    if $_issue_content {
+      file { '/etc/issue':
+        ensure  => file,
+        backup  => false,
+        content => $_issue_content,
+        }
+    }
+
+    if $_issue_net_content {
+      file { '/etc/issue.net':
+        ensure  => file,
+        backup  => false,
+        content => $_issue_net_content,
+        }
+    }
+
     if ($::osfamily == 'Debian') and ($dynamic_motd == false) {
       if $::operatingsystem == 'Debian' and versioncmp($::operatingsystemmajrelease, '7') > 0 {
         $_line_to_remove = 'session    optional     pam_motd.so  motd=/run/motd.dynamic'
