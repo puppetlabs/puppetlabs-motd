a802c7321dac0261a0fdfb29c6063993cd69f9a8
Helen Campbell <helen@puppet.com>
2018-05-18 15:37:49 +0100

    pdksync_heads/master-0-g34e3266

--- tests/beaker/tests/01_create_motd_content.rb
+++ tests/beaker/tests/01_create_motd_content.rb
@@ -14,7 +14,7 @@ site_pp = create_site_pp(master, manifest: pp)
 inject_site_pp(master, get_site_pp_path(master), site_pp)
 
 step 'Run Puppet Agent to apply the above manifest'
-confine_block(:except, roles: %w[master dashboard database]) do
+confine_block(:except, roles: ['master', 'dashboard', 'database']) do
   agents.each do |agent|
     on(agent, puppet('agent -t --environment production'), acceptable_exit_codes: [0, 2]) do |result|
       assert_no_match(%r{Error:}, result.stderr, 'Unexpected error was detected!')
