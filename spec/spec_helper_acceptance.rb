139f5db133ece4ef59d215e72a8f943175563675
David Swan <david.swan@puppet.com>
2019-03-01 15:34:57 +0000

    (FM-7799) - Cleanup of MOTD Testing - SpecInfra Implemented - Idempotent Apply Method Implemented - Code Coverage enabled on Unit test's

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,6 +12,20 @@ install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
+def idempotent_apply(hosts, manifest, opts = {}, &block)
+  block_on hosts, opts do |host|
+    file_path = host.tmpfile('apply_manifest.pp')
+    create_remote_file(host, file_path, manifest + "\n")
+
+    puppet_apply_opts = { :verbose => nil, 'detailed-exitcodes' => nil }
+    on_options = { acceptable_exit_codes: [0, 2] }
+    on host, puppet('apply', file_path, puppet_apply_opts), on_options, &block
+    puppet_apply_opts2 = { :verbose => nil, 'detailed-exitcodes' => nil }
+    on_options2 = { acceptable_exit_codes: [0] }
+    on host, puppet('apply', file_path, puppet_apply_opts2), on_options2, &block
+  end
+end
+
 RSpec.configure do |c|
   c.formatter = :documentation
   # Configure all nodes in nodeset
@@ -19,22 +33,20 @@ RSpec.configure do |c|
     run_puppet_access_login(user: 'admin') if pe_install? && (Gem::Version.new(puppet_version) >= Gem::Version.new('5.0.0'))
     hosts.each do |host|
       # This will be removed, this is temporary to test localisation.
-      if (fact('osfamily') == 'Debian' || fact('osfamily') == 'RedHat') &&
+      if (os[:family] == 'debian' || os[:family] == 'redhat') &&
          (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5') &&
           Gem::Version.new(puppet_version) < Gem::Version.new('5.2.0'))
         on(host, 'mkdir /opt/puppetlabs/puppet/share/locale/ja')
         on(host, 'touch /opt/puppetlabs/puppet/share/locale/ja/puppet.po')
       end
-      if fact('osfamily') == 'Debian'
+      if os[:family] == 'debian'
         # install language on debian systems
         install_language_on(host, 'ja_JP.utf-8') if not_controller(host)
         # This will be removed, this is temporary to test localisation.
       end
       # Required for binding tests.
-      if fact('osfamily') == 'RedHat'
-        if fact('operatingsystemmajrelease') =~ %r{7} || fact('operatingsystem') =~ %r{Fedora}
-          shell('yum install -y bzip2')
-        end
+      if os[:family] == 'redhat' && os[:release].start_with?('7')
+        shell('yum install -y bzip2')
       end
       on host, puppet('module', 'install', 'stahnma/epel')
     end
