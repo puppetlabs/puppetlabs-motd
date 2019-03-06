487e863fe3b93070d714bae833a7c071808650ed
tphoney <tp@puppet.com>
2019-03-06 17:05:01 +0000

    (feat) use puppet_litmus for acceptance testing

--- spec/acceptance/motd_spec.rb
+++ spec/acceptance/motd_spec.rb
@@ -62,7 +62,7 @@ PUPPETCODE
 # @param [string]  expected_contain:    Expected contents of the MOTD file to be compared
 # @param [string]  filename:            MOTD file to be tested
 def test_motd(pp, expected_contain, filename)
-  idempotent_apply(default, pp)
+  apply_manifest_and_idempotent(pp)
 
   return unless os[:family] != 'windows'
   expect(file(filename)).to be_file
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,54 +1,53 @@
-require 'beaker-pe'
-require 'beaker-puppet'
-require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
-require 'beaker/i18n_helper'
-require 'beaker-task_helper'
+# frozen_string_literal: true
 
-run_puppet_install_helper
-configure_type_defaults_on(hosts)
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
+require 'serverspec'
+require 'puppet_litmus'
+include PuppetLitmus
 
-def idempotent_apply(hosts, manifest, opts = {}, &block)
-  block_on hosts, opts do |host|
-    file_path = host.tmpfile('apply_manifest.pp')
-    create_remote_file(host, file_path, manifest + "\n")
-
-    puppet_apply_opts = { :verbose => nil, 'detailed-exitcodes' => nil }
-    on_options = { acceptable_exit_codes: [0, 2] }
-    on host, puppet('apply', file_path, puppet_apply_opts), on_options, &block
-    puppet_apply_opts2 = { :verbose => nil, 'detailed-exitcodes' => nil }
-    on_options2 = { acceptable_exit_codes: [0] }
-    on host, puppet('apply', file_path, puppet_apply_opts2), on_options2, &block
+if ENV['TARGET_HOST'].nil? || ENV['TARGET_HOST'] == 'localhost'
+  puts 'Running tests against this machine !'
+  if Gem.win_platform?
+    set :backend, :cmd
+  else
+    set :backend, :exec
   end
-end
+else
+  # load inventory
+  inventory_hash = inventory_hash_from_inventory_file
+  node_config = config_from_node(inventory_hash, ENV['TARGET_HOST'])
 
-RSpec.configure do |c|
-  c.formatter = :documentation
-  # Configure all nodes in nodeset
-  c.before :suite do
-    run_puppet_access_login(user: 'admin') if pe_install? && (Gem::Version.new(puppet_version) >= Gem::Version.new('5.0.0'))
-    hosts.each do |host|
-      # This will be removed, this is temporary to test localisation.
-      if (os[:family] == 'debian' || os[:family] == 'redhat') &&
-         (Gem::Version.new(puppet_version) >= Gem::Version.new('4.10.5') &&
-          Gem::Version.new(puppet_version) < Gem::Version.new('5.2.0'))
-        on(host, 'mkdir /opt/puppetlabs/puppet/share/locale/ja')
-        on(host, 'touch /opt/puppetlabs/puppet/share/locale/ja/puppet.po')
-      end
-      if os[:family] == 'debian'
-        # install language on debian systems
-        install_language_on(host, 'ja_JP.utf-8') if not_controller(host)
-        # This will be removed, this is temporary to test localisation.
-      end
-      # Required for binding tests.
-      if os[:family] == 'redhat' && os[:release].start_with?('7')
-        shell('yum install -y bzip2')
-      end
-      on host, puppet('module', 'install', 'stahnma/epel')
-    end
+  if target_in_group(inventory_hash, ENV['TARGET_HOST'], 'ssh_nodes')
+    set :backend, :ssh
+    options = Net::SSH::Config.for(host)
+    options[:user] = node_config.dig('ssh', 'user') unless node_config.dig('ssh', 'user').nil?
+    options[:port] = node_config.dig('ssh', 'port') unless node_config.dig('ssh', 'port').nil?
+    options[:password] = node_config.dig('ssh', 'password') unless node_config.dig('ssh', 'password').nil?
+    options[:verify_host_key] = Net::SSH::Verifiers::Null.new unless node_config.dig('ssh', 'host-key-check').nil?
+    host = if ENV['TARGET_HOST'].include?(':')
+             ENV['TARGET_HOST'].split(':').first
+           else
+             ENV['TARGET_HOST']
+           end
+    set :host,        options[:host_name] || host
+    set :ssh_options, options
+    set :request_pty, true
+  elsif target_in_group(inventory_hash, ENV['TARGET_HOST'], 'winrm_nodes')
+    require 'winrm'
+
+    set :backend, :winrm
+    set :os, family: 'windows'
+    user = node_config.dig('winrm', 'user') unless node_config.dig('winrm', 'user').nil?
+    pass = node_config.dig('winrm', 'password') unless node_config.dig('winrm', 'password').nil?
+    endpoint = "http://#{ENV['TARGET_HOST']}:5985/wsman"
+
+    opts = {
+      user: user,
+      password: pass,
+      endpoint: endpoint,
+      operation_timeout: 300,
+    }
+
+    winrm = WinRM::Connection.new opts
+    Specinfra.configuration.winrm = winrm
   end
 end
